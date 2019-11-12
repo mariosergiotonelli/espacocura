502a7ba6e262c21c058e187c71fb4742f132ab7b
Mario Tonelli <mariosergio_tonelli@hotmail.com>
2019-11-12 00:41:00 -0200

    Ajustando nova ideia home

--- espacocura/index.php
+++ espacocura/index.php
@@ -3,16 +3,15 @@
 
 <main class="wrapper">
   <section class="section-principal bg1">
-    <p class="text" >Conheça o Espaço</p>
   </section>
   <section class="section bg2">
-    <p class="text" >Yoga</p>
+    <p class="text" >Mussum Ipsum, cacilds vidis litro abertis. Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis. Casamentiss faiz malandris se pirulitá. Nec orci ornare consequat. Praesent lacinia ultrices consectetur. Sed non ipsum felis. Mé faiz elementum girarzis, nisi eros vermeio</p>
   </section>
   <section class="section bg3">
-    <p class="text" >Naturopatia</p>
+    <p class="text" >Mussum Ipsum, cacilds vidis litro abertis. Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis. Casamentiss faiz malandris se pirulitá. Nec orci ornare consequat. Praesent lacinia ultrices consectetur. Sed non ipsum felis. Mé faiz elementum girarzis, nisi eros vermeio</p>
   </section>
   <section class="section bg4">
-    <p class="text" >Sagrado Feminino</p>
+    <p class="text" >Mussum Ipsum, cacilds vidis litro abertis. Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis. Casamentiss faiz malandris se pirulitá. Nec orci ornare consequat. Praesent lacinia ultrices consectetur. Sed non ipsum felis. Mé faiz elementum girarzis, nisi eros vermeio</p>
   </section>
 
   <?php get_footer(); ?>
