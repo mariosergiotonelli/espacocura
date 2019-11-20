fe7c90b1ce364bed3e530287d87878686ca66c0d
Mario Tonelli <mariosergio_tonelli@hotmail.com>
2019-11-20 17:41:06 -0300

    Finalizando rodape

--- espacocura/footer.php
+++ espacocura/footer.php
@@ -1,62 +1,54 @@
-<footer class="footerFritz">
+<footer class="footerEspaco">
 
-  <section class="contatosFritz">
+  <section class="contatosEspaco">
     <div class="container">
       <div class="row">
-        <div class="logoFooter">
-          <a href="#" title="Fritz Alimentos">
-            <h1 class="logo">
-              <?php echo PW_SITE_NAME; ?>
-            </h1>
-          </a>
-        </div>
-
         <div class="infoRodape">
+
           <div class="endRodape">
-            <p><span class="glyphicon glyphicon-home"></span>Rua Gandu, 203 - Jardim Vale do Sol</p>
-            <p>São José dos Campos - SP</p>
-            <p>CEP 12238-040 </p>
-            <p><span class="glyphicon glyphicon-earphone"></span>(12) 3341-0162 - (12) 98299-4711</p>
+            <p><span class="glyphicon glyphicon-home"></span>Rua Agostinho Gomes, 2225 - Ipiranga</p>
+            <p>São Paulo - SP</p>
+            <p>CEP 04206-001 </p>
             <p><span class="glyphicon glyphicon-globe"></span>
-              <a href="mailto:faleconosco@fritzalimentos.com.br">faleconosco@fritzalimentos.com.br</a>
+              <a href="mailto:curaespaco.sp@gmail.com">curaespaco.sp@gmail.com</a>
             </p>
           </div>
 
           <div class="menuRodape">
             <ul>
-              <li><a href="<?php echo PW_URL ?>queijo"" class="menuRodapeLink">queijo crocante</a></li>
-              <li><a href="<?php echo PW_URL ?>produtos"" class="menuRodapeLink">produtos</a></li>
-              <li><a href="<?php echo PW_URL ?>historia"" class="menuRodapeLink">história</a></li>
+              <li><a href="<?php echo PW_URL ?>queijo" class="menuRodapeLink">queijo crocante</a></li>
+              <li><a href="<?php echo PW_URL ?>produtos" class="menuRodapeLink">produtos</a></li>
+              <li><a href="<?php echo PW_URL ?>historia" class="menuRodapeLink">história</a></li>
               <li><a href="<?php echo PW_URL ?>#PDV" class="menuRodapeLink">pontos de vendas</a></li>
               <li><a href="#contatoRodape" class="menuRodapeLink">contato</a></li>
-
             </ul>
           </div>
 
-        </div>
+          <div class="redesRodape">
+            <div class="menu__item">
+              <a target="_blank" class="menu__link redesHeader" href="https://www.facebook.com/espacocurasp">
+                <div class="facebook">facebook</div>
+              </a>
+            </div>
+            <div class="menu__item">
+              <a target="_blank" class="menu__link redesHeader" href="https://www.instagram.com/curaespaco/">
+                <div class="instagram">instagram</div>
+              </a>
+            </div>
+          </div>
 
-        <div class="contatoRodape" id="contatoRodape">
-          <h4 class="contatoRodape__titulo">
-            Entre em contato
-          </h4>
-          <p class="contatoRodape__descricao">
-            Estamos dispostos a atende-lo.
-          </p>
-          <?php echo do_shortcode('[contact-form-7 id="4" title="Contato"]'); ?>
         </div>
       </div>
     </div>
   </section>
 
-  <section class="direitosFritz">
+  <section class="direitosEspaco">
     <div class="container">
       <div class="row">
-        <p>© Todos os direitos reservados à Fritz@ Alimentos </p>
+        <p>© Todos os direitos reservados à Espaco Cura </p>
         <p>Desenvolvido por <a href="http://mariotonelli.com.br">Mário Tonelli</a></p>
       </div>
-
     </div>
-
   </section>
 
 </footer>
--- espacocura/index.php
+++ espacocura/index.php
@@ -1,8 +1,7 @@
 <?php get_header(); ?>
 
 
-<main class="wrapper">
-
+<main class="mainEspaco">
   <div class="lateralEsquerda animated fadeInLeft"></div>
   <div class="lateralDireita animated fadeInRight"></div>
 
@@ -10,37 +9,40 @@
 
   <section class="yoga">
     <div class="yoga-left">
-      <img class="yoga-left__simbolo animated fadeIn" src="<?php echo PW_URL_THEME ?>/assets/img/yoga.png" alt="Yoga">
+      <img class="yoga-left__simbolo animated fadeIn" src="<?php echo PW_URL_THEME ?>/assets/img/mandala-branca.png" alt="Mandala Espaço Cura">
     </div>
     <div class="yoga-right animated slideInRight">
-      <p>Mussum Ipsum, cacilds vidis litro abertis. Mauris nec dolor in eros commodo tempor. Aenean aliquam molestie leo, vitae iaculis nisl. Delegadis gente finis, bibendum egestas augue arcu ut est. Manduma pindureta quium dia nois paga. Mais vale um bebadis conhecidiss, que um alcoolatra anonimis.</p>
-      <p>Mé faiz elementum girarzis, nisi eros vermeio. Todo mundo vê os porris que eu tomo, mas ninguém vê os tombis que eu levo! Diuretics paradis num copo é motivis de denguis. A ordem dos tratores não altera o pão duris.</p>
-      <p>Mussum Ipsum, cacilds vidis litro abertis. Mauris nec dolor in eros commodo tempor. Aenean aliquam molestie leo, vitae iaculis nisl. Delegadis gente finis, bibendum egestas augue arcu ut est. Manduma pindureta quium dia nois paga. Mais vale um bebadis conhecidiss, que um alcoolatra anonimis.</p>
-      <p>Mé faiz elementum girarzis, nisi eros vermeio. Todo mundo vê os porris que eu tomo, mas ninguém vê os tombis que eu levo! Diuretics paradis num copo é motivis de denguis. A ordem dos tratores não altera o pão duris.</p>
+      <div class="yoga-right__titulo">Yoga</div>
+      <div class="yoga-right__content">
+        <p class="yoga-right__texto">Mussum Ipsum, cacilds vidis litro abertis. Mauris nec dolor in eros commodo tempor. Aenean aliquam molestie leo, vitae iaculis nisl. Delegadis gente finis, bibendum egestas augue arcu ut est. Manduma pindureta quium dia nois paga. Mais vale um bebadis conhecidiss, que um alcoolatra anonimis.</p>
+        <p class="yoga-right__texto">Mé faiz elementum girarzis, nisi eros vermeio. Todo mundo vê os porris que eu tomo, mas ninguém vê os tombis que eu levo! Diuretics paradis num copo é motivis de denguis. A ordem dos tratores não altera o pão duris.</p>
+      </div>
     </div>
   </section>
 
-  <section class="naturopatia fadeInUp">
-    <div class="naturopatia-left fadeInUp">
-      <p>Mussum Ipsum, cacilds vidis litro abertis. Mauris nec dolor in eros commodo tempor. Aenean aliquam molestie leo, vitae iaculis nisl. Delegadis gente finis, bibendum egestas augue arcu ut est. Manduma pindureta quium dia nois paga. Mais vale um bebadis conhecidiss, que um alcoolatra anonimis.</p>
-      <p>Mé faiz elementum girarzis, nisi eros vermeio. Todo mundo vê os porris que eu tomo, mas ninguém vê os tombis que eu levo! Diuretics paradis num copo é motivis de denguis. A ordem dos tratores não altera o pão duris.</p>
-      <p>Mussum Ipsum, cacilds vidis litro abertis. Mauris nec dolor in eros commodo tempor. Aenean aliquam molestie leo, vitae iaculis nisl. Delegadis gente finis, bibendum egestas augue arcu ut est. Manduma pindureta quium dia nois paga. Mais vale um bebadis conhecidiss, que um alcoolatra anonimis.</p>
-      <p>Mé faiz elementum girarzis, nisi eros vermeio. Todo mundo vê os porris que eu tomo, mas ninguém vê os tombis que eu levo! Diuretics paradis num copo é motivis de denguis. A ordem dos tratores não altera o pão duris.</p>
+  <section class="naturopatia">
+    <div class="naturopatia-left animated slideInLeft">
+      <div class="naturopatia-left__titulo">naturopatia</div>
+        <div class="naturopatia-left__content">
+          <p class="naturopatia-left__texto">Mussum Ipsum, cacilds vidis litro abertis. Mauris nec dolor in eros commodo tempor. Aenean aliquam molestie leo, vitae iaculis nisl. Delegadis gente finis, bibendum egestas augue arcu ut est. Manduma pindureta quium dia nois paga. Mais vale um bebadis conhecidiss, que um alcoolatra anonimis.</p>
+          <p class="naturopatia-left__texto">Mé faiz elementum girarzis, nisi eros vermeio. Todo mundo vê os porris que eu tomo, mas ninguém vê os tombis que eu levo! Diuretics paradis num copo é motivis de denguis. A ordem dos tratores não altera o pão duris.</p>
+        </div>
     </div>
-    <div class="naturopatia-right">
-      <img class="naturopatia-right__simbolo " src="<?php echo PW_URL_THEME ?>/assets/img/yoga.png" alt="naturopatia">
+    <div class="naturopatia-right animated slideInLeft">
+      <img class="naturopatia-right__simbolo animated fadeIn" src="<?php echo PW_URL_THEME ?>/assets/img/mandala-branca.png" alt="Mandala Espaço Cura">
     </div>
   </section>
 
   <section class="sagrado">
     <div class="sagrado-left">
-      <img class="sagrado-left__simbolo" src="<?php echo PW_URL_THEME ?>/assets/img/yoga.png" alt="sagrado">
+      <img class="sagrado-left__simbolo animated fadeIn" src="<?php echo PW_URL_THEME ?>/assets/img/mandala-branca.png" alt="Mandala Espaço Cura">
     </div>
-    <div class="sagrado-right">
-    <p>Mussum Ipsum, cacilds vidis litro abertis. Mauris nec dolor in eros commodo tempor. Aenean aliquam molestie leo, vitae iaculis nisl. Delegadis gente finis, bibendum egestas augue arcu ut est. Manduma pindureta quium dia nois paga. Mais vale um bebadis conhecidiss, que um alcoolatra anonimis.</p>
-      <p>Mé faiz elementum girarzis, nisi eros vermeio. Todo mundo vê os porris que eu tomo, mas ninguém vê os tombis que eu levo! Diuretics paradis num copo é motivis de denguis. A ordem dos tratores não altera o pão duris.</p>
-      <p>Mussum Ipsum, cacilds vidis litro abertis. Mauris nec dolor in eros commodo tempor. Aenean aliquam molestie leo, vitae iaculis nisl. Delegadis gente finis, bibendum egestas augue arcu ut est. Manduma pindureta quium dia nois paga. Mais vale um bebadis conhecidiss, que um alcoolatra anonimis.</p>
-      <p>Mé faiz elementum girarzis, nisi eros vermeio. Todo mundo vê os porris que eu tomo, mas ninguém vê os tombis que eu levo! Diuretics paradis num copo é motivis de denguis. A ordem dos tratores não altera o pão duris.</p>
+    <div class="sagrado-right animated slideInRight">
+      <div class="sagrado-right__titulo">Sagrado Feminino</div>
+      <div class="sagrado-right__content">
+        <p class="sagrado-right__texto">Mussum Ipsum, cacilds vidis litro abertis. Mauris nec dolor in eros commodo tempor. Aenean aliquam molestie leo, vitae iaculis nisl. Delegadis gente finis, bibendum egestas augue arcu ut est. Manduma pindureta quium dia nois paga. Mais vale um bebadis conhecidiss, que um alcoolatra anonimis.</p>
+        <p class="sagrado-right__texto">Mé faiz elementum girarzis, nisi eros vermeio. Todo mundo vê os porris que eu tomo, mas ninguém vê os tombis que eu levo! Diuretics paradis num copo é motivis de denguis. A ordem dos tratores não altera o pão duris.</p>
+      </div>
     </div>
   </section>
 
