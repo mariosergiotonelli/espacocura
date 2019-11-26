e3bca502f20d2a7c882d28061a865521630b5a07
Mario Tonelli <mariosergio_tonelli@hotmail.com>
2019-11-26 01:07:12 -0300

    FInalizando estruturas de pagina

--- espacocura/page-espaco.php
+++ espacocura/page-espaco.php
@@ -8,30 +8,26 @@ get_header();
 
 <main class="mainPageEspaco">
 
-  <div class="mainPageEspaco-topo"></div>
+  <div class="mainPageEspaco-imgHeader">
+    <img class="mainPageEspaco-imgHeader__desktop" src="<?php echo PW_URL_THEME ?>/assets/img/espaco/o-espaco-header.jpg" alt="Espaço Cura">
+    <img class="mainPageEspaco-imgHeader__mobile" src="<?php echo PW_URL_THEME ?>/assets/img/espaco/o-espaco-header-mobile.png" alt="Espaço Cura">
+  </div>
 
   <div class="container">
-    <div class="row">
-      <h1 class="mainPageEspaco-titulo">Espaço Cura</h1>
-      <h4 class="mainPageEspaco-subtitulo">Mussum Ipsum, cacilds vidis litro abertis. Quem num gosta di mé, boa gentis num é. Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis. Si num tem leite então bota uma pinga aí cumpadi! Manduma pindureta quium dia nois paga.</h4>
-    </div>
 
     <div class="row">
       <div class="mainPageEspaco-content">
-        <div class="mainPageEspaco-content__servicos">
-          <div class="mainPageEspaco-content__servicos-img"></div>
-          <div class="mainPageEspaco-content__servicos-titulo">Denise de Cássia</div>
-          <div class="mainPageEspaco-content__servicos-texto">Mussum Ipsum, cacilds vidis litro abertis. Quem num gosta di mé, boa gentis num é. Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis. Si num tem leite então bota uma pinga aí cumpadi! Manduma pindureta quium dia nois paga.</div>
+        <div class="mainPageEspaco-content__texto">
+          Mussum Ipsum, cacilds vidis litro abertis. Admodum accumsan disputationi eu sit. Vide electram sadipscing et per. Casamentiss faiz malandris se pirulitá. Em pé sem cair, deitado sem dormir, sentado sem cochilar e fazendo pose. Quem num gosta di mé, boa gentis num é.
         </div>
-        <div class="mainPageEspaco-content__servicos">
-          <div class="mainPageEspaco-content__servicos-img"></div>
-          <div class="mainPageEspaco-content__servicos-titulo">Raisa Covre</div>
-          <div class="mainPageEspaco-content__servicos-texto">Mussum Ipsum, cacilds vidis litro abertis. Quem num gosta di mé, boa gentis num é. Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis. S</div>
+        <div class="mainPageEspaco-content__texto">
+          Quem manda na minha terra sou euzis! Vehicula non. Ut sed ex eros. Vivamus sit amet nibh non tellus tristique interdum. Per aumento de cachacis, eu reclamis. Viva Forevis aptent taciti sociosqu ad litora torquent.
         </div>
-        <div class="mainPageEspaco-content__servicos">
-          <div class="mainPageEspaco-content__servicos-img"></div>
-          <div class="mainPageEspaco-content__servicos-titulo">Raquel Amaral</div>
-          <div class="mainPageEspaco-content__servicos-texto">Mussum Ipsum, cacilds vidis litro abertis. Quem num gosta di mé, boa gentis num é. Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis. Si num tem leite então bota uma pinga aí cumpadi.</div>
+        <div class="mainPageEspaco-content__texto">
+          Si num tem leite então bota uma pinga aí cumpadi! Delegadis gente finis, bibendum egestas augue arcu ut est. Interagi no mé, cursus quis, vehicula ac nisi. In elementis mé pra quem é amistosis quis leo.
+        </div>
+        <div class="mainPageEspaco-content__texto">
+          Paisis, filhis, espiritis santis. Copo furadis é disculpa de bebadis, arcu quam euismod magna. Si u mundo tá muito paradis? Toma um mé que o mundo vai girarzis! Nec orci ornare consequat. Praesent lacinia ultrices consectetur. Sed non ipsum felis.
         </div>
       </div>
     </div>
