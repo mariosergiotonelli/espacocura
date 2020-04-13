1c6e2ac2dc329c7ce5e75a3012d7487d08acc111
Mário Tonelli <mario.tonelli@profite.com.br>
2020-04-12 21:34:13 -0300

    Ajustando chamada de arquivos no header

--- espacocura/header.php
+++ espacocura/header.php
@@ -4,8 +4,8 @@
 <title><?php wp_title( ' | ', true, 'right');?></title>
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1" />
-<link rel="stylesheet" href="<?php echo PW_URL_THEME; ?>/espacocura/assets/css/style.css" />
-<link rel="stylesheet" href="<?php echo PW_URL_THEME; ?>/espacocura//assets/css/reset.css" />
+<link rel="stylesheet" href="<?php echo PW_URL_THEME; ?>/assets/css/style.css" />
+<link rel="stylesheet" href="<?php echo PW_URL_THEME; ?>/assets/css/reset.css" />
 <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
 <link rel="stylesheet" href="//cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick.css"/>
